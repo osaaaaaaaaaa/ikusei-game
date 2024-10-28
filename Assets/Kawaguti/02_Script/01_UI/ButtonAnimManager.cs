33f4b1b5522f14f40f3b1ae3579abd8035b40eaa
S-JOBI23-054\student <231076@st.yoshida.-g.ac.jp>
2024-10-28 11:47:51 +0900

    k-kawaguchi トップシーンのUI作成

--- Assets/Kawaguti/02_Script/01_UI/ButtonAnimManager.cs
+++ Assets/Kawaguti/02_Script/01_UI/ButtonAnimManager.cs
@@ -10,6 +10,8 @@ public class ButtonAnimManager : MonoBehaviour
 
     Animator animator;
 
+    [SerializeField] GameObject TapgGuard;
+
     [SerializeField] GameObject MenuButton;
     [SerializeField] GameObject CloseButton;
 
@@ -23,6 +25,8 @@ public class ButtonAnimManager : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        TapgGuard.SetActive(false);
 
         MenuButton.SetActive(true);
         CloseButton.SetActive(false);
@@ -42,14 +46,34 @@ public class ButtonAnimManager : MonoBehaviour
         //menu.SetActive(true);
         animator.SetBool("OnClickButton", true);
         CloseButton.SetActive(true);
+
+        TapgGuard.SetActive(true);
+
+        //ŠeŽíƒ{ƒ^ƒ“
+        LibButton.SetActive(true);
+        ShopButton.SetActive(true);
+        GrowButton.SetActive(true);
+        MixButton.SetActive(true);
+        ItemButton.SetActive(true);
     }
 
     public void CloseMenuAnim()
     {
         animator.SetBool("OnClickButton", false);
         CloseButton.SetActive(false);
+        Invoke("SetOnActiveButton", 0.5f);
     }
 
     //
+    private void SetOnActiveButton()
+    {
+        TapgGuard.SetActive(false);
+
+        LibButton.SetActive(false);
+        ShopButton.SetActive(false);
+        GrowButton.SetActive(false);
+        MixButton.SetActive(false);
+        ItemButton.SetActive(false);
+    }
 
 }
