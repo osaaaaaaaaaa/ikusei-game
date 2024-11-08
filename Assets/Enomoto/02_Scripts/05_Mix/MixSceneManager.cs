024da5727febc677ad6f3b226c0ccbef6ee3bce2
S-JOBI23-055\student <231105@st.yoshida-g.ac.jp>
2024-11-08 13:26:21 +0900

    背景の調整。

--- Assets/Enomoto/02_Scripts/05_Mix/MixSceneManager.cs
+++ Assets/Enomoto/02_Scripts/05_Mix/MixSceneManager.cs
@@ -15,7 +15,7 @@ public class MixSceneManager : MonoBehaviour
     [SerializeField] Transform monsterPoint;
     [SerializeField] GameObject eggPrefab;
     GameObject monster;
-    const int mixLimitNum = 8;
+    const int mixLimitNum = 5;
 
     // Start is called before the first frame update
     void Start()
